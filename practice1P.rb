5ca335dd11593af42f5b88f9cf0308f829a606c7
yuriyanagi <40306yy@gmail.com>
2020-08-31 23:33:28 +0900

    Create practice1P.rb

---
+++ practice1P.rb
@@ -0,0 +1,11 @@
+number = 10
+
+if number % 3 == 0 && number % 5 ==0
+  puts "15の倍数です"
+elsif number % 5 ==0
+  puts "5の倍数です"
+elsif number % 3 ==0
+  puts "3の倍数です"
+else
+  puts "3の倍数でも5の倍数でもありません"
+end
