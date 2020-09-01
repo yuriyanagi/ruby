442954d9354741d2fe0005f6cadb043702a3aa80
yuriyanagi <40306yy@gmail.com>
2020-09-02 00:14:01 +0900

    practice

--- practice4drinkP.rb
+++ practice4drinkP.rb
@@ -2,4 +2,8 @@ require "./practice4menuP"
 
 class Drink < Menu
   attr_accessor :amount
+
+  def info
+    return "#{self.name} #{self.price}円(#{self.amount}mL)"
+  end
 end
--- practice4foodP.rb
+++ practice4foodP.rb
@@ -3,6 +3,10 @@ require "./practice4menuP"
 class Food < Menu
   attr_accessor :calorie
 
+  def info
+    return "#{self.name} #{self.price}円(#{self.calorie}ckal)"
+  end
+
   def calorie_info
     return "#{self.name}は#{self.calorie}kcalです"
   end
