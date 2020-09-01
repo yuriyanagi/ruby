e2277e069d98f85f80d46329fb5506358f45de1e
yuriyanagi <40306yy@gmail.com>
2020-09-01 17:19:13 +0900

    Update practice3P.rb

--- practice3P.rb
+++ practice3P.rb
@@ -1,3 +1,4 @@
+# 7
 # def discount(price)
 #   return price / 2
 # end
@@ -7,13 +8,28 @@
 # puts "特別価格で#{half_price}円です"
 
 
+# 8
+# def shopping_free?(price)
+#   return price >= 5000
+# end
 
-def shopping_free?(price)
-  return price >= 5000
+# if shopping_free?(10000)
+#   puts "5000円以上のお買い上げなので送料はいただきません"
+# else
+#   puts "追加で送料をいただきます"
+# end
+
+
+# 9
+def price_with_shipping(price)
+  if price >=5000
+    return price
+  end
+  return price +500
 end
 
-if shopping_free?(10000)
-  puts "5000円以上のお買い上げなので送料はいただきません"
-else
-  puts "追加で送料をいただきます"
-end
+puts "商品の合計金額は3000円です"
+puts "お支払い金額は、送料込みで#{price_with_shipping(3000)}円です"
+puts "-----------"
+puts "商品の合計金額は10000円です"
+puts "お支払い金額は、送料込みで#{price_with_shipping(10000)}円です"
