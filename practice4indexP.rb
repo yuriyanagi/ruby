b332b3b298fb9e273d6fb6ed729d046de7592fb9
yuriyanagi <40306yy@gmail.com>
2020-09-01 23:47:14 +0900

    Update practice4indexP.rb

--- practice4indexP.rb
+++ practice4indexP.rb
@@ -2,35 +2,30 @@ require "./practice4foodP"
 require "./practice4drinkP"
 
 food1 = Food.new(name:"ピザ", price:800)
-food1.calorie = 700
-
-puts food1.calorie_info
-# food2 = Food.new(name:"すし", price:1000)
+food2 = Food.new(name:"すし", price:1000)
 drink1 = Drink.new(name:"コーラ", price:300)
-drink1.amount = 500
-puts drink1.amount
-# drink2 = Drink.new(name:"お茶", price:200)
+drink2 = Drink.new(name:"お茶", price:200)
 
-# menus = [menu1, menu2, menu3, menu4]
+menus = [food1, food2, drink1, drink2]
 
-# index = 0
+index = 0
 
-# menus.each do |menu|
-#   puts "#{index}.#{menu.info}"
-#   index += 1
-# end
+menus.each do |menu|
+  puts "#{index}.#{menu.info}"
+  index += 1
+end
 
-# puts "--------------"
-# puts "メニューの番号を選択してください"
+puts "--------------"
+puts "メニューの番号を選択してください"
 
-# order = gets.chomp.to_i
+order = gets.chomp.to_i
 
-# selected_menu = menus[order]
+selected_menu = menus[order]
 
-# puts "選択されたメニュー：#{selected_menu.name}"
+puts "選択されたメニュー：#{selected_menu.name}"
 
-# puts "個数を入力してください(3つ以上で100円割引)"
+puts "個数を入力してください(3つ以上で100円割引)"
 
-# count = gets.chomp.to_i
+count = gets.chomp.to_i
 
-# puts "お会計は#{selected_menu.get_total_price(count)}円です"
+puts "お会計は#{selected_menu.get_total_price(count)}円です"
