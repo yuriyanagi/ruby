c9e7f7af7ab2b9d02e4e5c354b300c1aa5e1764d
yuriyanagi <40306yy@gmail.com>
2020-09-01 22:20:36 +0900

    Update practice4indexP.rb

--- practice4indexP.rb
+++ practice4indexP.rb
@@ -13,3 +13,18 @@ menus.each do |menu|
   puts "#{index}.#{menu.info}"
   index += 1
 end
+
+puts "--------------"
+puts "メニューの番号を選択してください"
+
+order = gets.chomp.to_i
+
+selected_menu = menus[order]
+
+puts "選択されたメニュー：#{selected_menu.name}"
+
+puts "個数を入力してください(3つ以上で100円割引)"
+
+count = gets.chomp.to_i
+
+puts "お会計は#{selected_menu.get_total_price(count)}円です"
