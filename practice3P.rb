846509ec1c07a74adcb65916e69dccc168c97ca5
yuriyanagi <40306yy@gmail.com>
2020-09-01 17:11:24 +0900

    Update practice3P.rb

--- practice3P.rb
+++ practice3P.rb
@@ -1,7 +1,19 @@
-def discount(price)
-  return price / 2
+# def discount(price)
+#   return price / 2
+# end
+
+# half_price = discount(15000)
+
+# puts "特別価格で#{half_price}円です"
+
+
+
+def shopping_free?(price)
+  return price >= 5000
 end
 
-half_price = discount(15000)
-
-puts "特別価格で#{half_price}円です"
+if shopping_free?(10000)
+  puts "5000円以上のお買い上げなので送料はいただきません"
+else
+  puts "追加で送料をいただきます"
+end
