d851edc06136fbe0952f5536c1bc1ade7e8e2e06
yuriyanagi <40306yy@gmail.com>
2020-09-01 21:24:03 +0900

    Update practice4P.rb

--- practice4P.rb
+++ practice4P.rb
@@ -2,6 +2,11 @@ class Menu
   attr_accessor :name
   attr_accessor :price
 
+  def initialize
+    self.name = "ピザ"
+    self.price = 800
+  end
+
   def info
     return "#{self.name} #{self.price}円"
   end
@@ -17,7 +22,4 @@ end
 
 menu1 = Menu.new
 
-menu1.name = "ピザ"
-menu1.price = 800
-
-puts menu1.get_total_price(3)
+puts menu1.info
