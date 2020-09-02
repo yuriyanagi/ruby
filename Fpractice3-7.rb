0b0ef89cdd6819431895ba59d79b9dac286a164e
yuriyanagi <40306yy@gmail.com>
2020-09-02 23:00:19 +0900

    Update Fpractice3-7.rb

--- Fpractice3-7.rb
+++ Fpractice3-7.rb
@@ -1,9 +1,9 @@
-team = ["A", "B", "C", "D"]
+team = ["A", "B", "C", "D","E"]
+opps = ["A", "B", "C", "D","E"]
 
 team.each do |t1|
-  team.each do |t2|
-    if t1 != t2
-      puts t1 + "vs" + t2
-    end
+  opps.shift
+  opps.each do |t2|
+    puts t1 + "vs" + t2
   end
 end
