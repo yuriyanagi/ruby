1986b416b05477041ecb61c59ef52fcdef1c25a8
yuriyanagi <40306yy@gmail.com>
2020-09-04 01:20:08 +0900

    Update CpracticeClass.rb

--- CpracticeClass.rb
+++ CpracticeClass.rb
@@ -1,14 +1,25 @@
 class Car
+  def initialize(car_name, car_model, capacity)
+    @name = car_name
+    @model = car_model
+    @capacity = capacity
+  end
+
   def Car.run
     puts "走行します"
   end
 
   def ride_car(ride_number)
     @ride_number = ride_number
+    if ride_number > @capacity
+      puts "#{@name}は、乗車定員が#{@capacity}なので、#{@ride_number}は乗れません。"
+    else
+      puts "#{ride_number}人乗車できました。"
+    end
   end
 end
 
-fire_truck = Car.new
+fire_truck = Car.new("消防車", "大型車", 5)
 
 fire_truck.ride_car(3)
 
