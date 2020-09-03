694da5db0a2f8b2a932083cd57ddf5908bf29dce
yuriyanagi <40306yy@gmail.com>
2020-09-03 17:25:15 +0900

    Update CpracticeRollerCoaster.rb

--- CpracticeRollerCoaster.rb
+++ CpracticeRollerCoaster.rb
@@ -1,22 +1,31 @@
-ride_count = 0
-friends =[]
+def judge_height(list)
+  ride_count = 0
 
-3.times do
+  list.each do |friend|
+    if friend[:height] >= 130
+      puts "#{friend[:name]}さんは乗車できます"
+      ride_count += 1
+    else
+      puts "#{friend[:name]}さんは乗車できません！"
+    end
+  end
+  
+  puts "乗車するのは#{ride_count}人です"
+end
+
+def add_friend(list)
   friend = {}
   puts "お友達に名前は？"
   friend[:name] = gets.chomp
   puts "お友達の身長は？"
   friend[:height] = gets.to_i
-  friends << friend
+  list << friend
 end
 
-friends.each do |friend|
-  if friend[:height] >= 130
-    puts "#{friend[:name]}さんは乗車できます"
-    ride_count += 1
-  else
-    puts "#{friend[:name]}さんは乗車できません！"
-  end
+friends =[]
+
+3.times do
+  add_friend(friends)
 end
 
-puts "乗車するのは#{ride_count}人です"
+judge_height(friends)
