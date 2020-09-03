9509f90deee0f86dac083f379be36aa62aecdc04
yuriyanagi <40306yy@gmail.com>
2020-09-03 16:22:57 +0900

    Update CpracticeRollerCoaster.rb

--- CpracticeRollerCoaster.rb
+++ CpracticeRollerCoaster.rb
@@ -1,43 +1,19 @@
 ride_count = 0
 friends =[]
 
-friend = {}
-puts "お友達に名前は？"
-friend[:name] = gets.chomp
-puts "お友達の身長は？"
-friend[:height] = gets.to_i
-friends << friend
-if friend[:height] >= 130
-  puts "#{friend[:name]}さんは乗車できます"
-  ride_count += 1
-else
-  puts "#{friend[:name]}さんは乗車できません！"
-end
-
-friend = {}
-puts "お友達に名前は？"
-friend[:name] = gets.chomp
-puts "お友達の身長は？"
-friend[:height] = gets.to_i
-friends << friend
-if friend[:height] >= 130
-  puts "#{friend[:name]}さんは乗車できます"
-  ride_count += 1
-else
-  puts "#{friend[:name]}さんは乗車できません！"
-end
-
-friend = {}
-puts "お友達に名前は？"
-friend[:name] = gets.chomp
-puts "お友達の身長は？"
-friend[:height] = gets.to_i
-friends << friend
-if friend[:height] >= 130
-  puts "#{friend[:name]}さんは乗車できます"
-  ride_count += 1
-else
-  puts "#{friend[:name]}さんは乗車できません！"
+3.times do
+  friend = {}
+  puts "お友達に名前は？"
+  friend[:name] = gets.chomp
+  puts "お友達の身長は？"
+  friend[:height] = gets.to_i
+  friends << friend
+  if friend[:height] >= 130
+    puts "#{friend[:name]}さんは乗車できます"
+    ride_count += 1
+  else
+    puts "#{friend[:name]}さんは乗車できません！"
+  end
 end
 
 puts "乗車するのは#{ride_count}人です"
