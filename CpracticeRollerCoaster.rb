36c8161d37d41d49311647d3778684792c1c2e57
yuriyanagi <40306yy@gmail.com>
2020-09-03 15:26:23 +0900

    Create CpracticeRollerCoaster.rb

---
+++ CpracticeRollerCoaster.rb
@@ -0,0 +1,22 @@
+ride_count = 0
+friends =[]
+
+puts "お友達に名前は？"
+friends = gets.chomp
+puts "お友達の身長は？"
+friends = gets.to_i
+ride_count += 1
+
+puts "お友達に名前は？"
+friends = gets.chomp
+puts "お友達の身長は？"
+friends = gets.to_i
+ride_count += 1
+
+puts "お友達に名前は？"
+friends = gets.chomp
+puts "お友達の身長は？"
+friends = gets.to_i
+ride_count += 1
+
+puts "乗車するのは#{ride_count}人です"
