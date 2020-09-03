5c88678f95aa9531f143f64f7b5eaa40285353a9
yuriyanagi <40306yy@gmail.com>
2020-09-03 16:22:03 +0900

    Update CpracticeRollerCoaster.rb

--- CpracticeRollerCoaster.rb
+++ CpracticeRollerCoaster.rb
@@ -7,7 +7,12 @@ friend[:name] = gets.chomp
 puts "お友達の身長は？"
 friend[:height] = gets.to_i
 friends << friend
-ride_count += 1
+if friend[:height] >= 130
+  puts "#{friend[:name]}さんは乗車できます"
+  ride_count += 1
+else
+  puts "#{friend[:name]}さんは乗車できません！"
+end
 
 friend = {}
 puts "お友達に名前は？"
@@ -15,7 +20,12 @@ friend[:name] = gets.chomp
 puts "お友達の身長は？"
 friend[:height] = gets.to_i
 friends << friend
-ride_count += 1
+if friend[:height] >= 130
+  puts "#{friend[:name]}さんは乗車できます"
+  ride_count += 1
+else
+  puts "#{friend[:name]}さんは乗車できません！"
+end
 
 friend = {}
 puts "お友達に名前は？"
@@ -23,6 +33,11 @@ friend[:name] = gets.chomp
 puts "お友達の身長は？"
 friend[:height] = gets.to_i
 friends << friend
-ride_count += 1
+if friend[:height] >= 130
+  puts "#{friend[:name]}さんは乗車できます"
+  ride_count += 1
+else
+  puts "#{friend[:name]}さんは乗車できません！"
+end
 
 puts "乗車するのは#{ride_count}人です"
