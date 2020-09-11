d59e21a8c160267b97e9f530770b5ae4d0353e74
yuriyanagi <40306yy@gmail.com>
2020-09-11 16:30:02 +0900

    Update 10.rb

--- drill/10.rb
+++ drill/10.rb
@@ -1,1 +1,12 @@
+def search(num,input)
+  input.each_with_index do |item, i|
+    if item == num
+      puts "#{i + 1}番目にあります"
+      return
+    end
+  end
+  puts "その数は含まれていません"
+end
+
 input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
+search(９,input)
