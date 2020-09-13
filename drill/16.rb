9af2f50bd7b915954d9602f7b3705e7fff1a79a3
yuriyanagi <40306yy@gmail.com>
2020-09-13 18:40:03 +0900

    Update 16.rb

--- drill/16.rb
+++ drill/16.rb
@@ -1,8 +1,20 @@
 class Fruits
   # ここにクラスの定義を書き加えてください
+  def initialize(name, price)
+    @name = name
+    @price = price
+    @sum = 
+  end
+
+  def get_sum
+    puts "合計の価格は#{}円です"
+  end
 end
 
-
 # 以下で3つのインスタンスを生成してください
+apple = Fruits.new("リンゴ", 120)
+orange = Fruits.new("オレンジ", 200)
+straberry = Fruits.new("イチゴ",60)
 
 # 生成したらクラスメソッドget_sumを呼んで合計価格を表示しましょう
+get_sum()
