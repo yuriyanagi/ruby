1fcc9be7e7b224e3835045bc680aba7e55c8d684
yuriyanagi <40306yy@gmail.com>
2020-09-13 18:15:50 +0900

    Update 14.rb

--- drill/14.rb
+++ drill/14.rb
@@ -1,10 +1,10 @@
 def in1to10(num, outside_mode)
-  num_judge = num >= 1 || num <= 10
-  if outside_mode && num_judge = outside_mode
+  num_judge = num >= 1 && num <= 10
+  if outside_mode || num_judge
     puts "True"
   else
     puts "False"
   end
 end
 
-in1to10(5,false)
+in1to10(11,true)
