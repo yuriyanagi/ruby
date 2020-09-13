303139663e608813fda1eb98f8a9c158f8018c7d
yuriyanagi <40306yy@gmail.com>
2020-09-13 18:55:15 +0900

    Update 16.rb

--- drill/16.rb
+++ drill/16.rb
@@ -1,13 +1,15 @@
 class Fruits
   # ここにクラスの定義を書き加えてください
+  @@sum = 0
+
   def initialize(name, price)
     @name = name
     @price = price
-    @sum = 
+    @@sum = @@sum + price
   end
 
-  def get_sum
-    puts "合計の価格は#{}円です"
+  def self.get_sum
+    puts "合計の価格は#{@@sum}円です"
   end
 end
 
@@ -17,4 +19,4 @@ orange = Fruits.new("オレンジ", 200)
 straberry = Fruits.new("イチゴ",60)
 
 # 生成したらクラスメソッドget_sumを呼んで合計価格を表示しましょう
-get_sum()
+Fruits.get_sum
