7403448cdc018e911021b318af9bf2c15aa46641
yuriyanagi <40306yy@gmail.com>
2020-09-23 23:52:17 +0900

    Update 29.rb

--- drill/29.rb
+++ drill/29.rb
@@ -1,5 +1,16 @@
 def fizz_buzz
-  
+  100.each do |num|
+    if num % 15 == 0
+      puts "FizzBuzz"
+      if num % 3 == 0
+        puts "Fizz"
+      elsif num % 5 == 0
+        puts "Buzz"
+      end
+    else
+      puts num
+    end
+  end
 end
 
 fizz_buzz
