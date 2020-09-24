47179113294977423f452ffd4f246aa42b41a5c4
yuriyanagi <40306yy@gmail.com>
2020-09-24 17:47:03 +0900

    Update 30.rb

--- drill/30.rb
+++ drill/30.rb
@@ -1,12 +1,14 @@
 def fizzbuzz(max_num)
-  if 
-
-  elsif
-
-  elsif
-
-  else
-
+  (1..max_num).each do |num|
+    if (num % 3 == 0) && (num % 5 == 0)
+      puts "FizzBuzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      puts num
+    end
   end
 end
 
