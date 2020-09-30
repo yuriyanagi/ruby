7844c139cf6b80c18b28b425297d193864a6d8be
yuriyanagi <40306yy@gmail.com>
2020-10-01 02:15:16 +0900

    Update 31.rb

--- drill/31.rb
+++ drill/31.rb
@@ -1,5 +1,13 @@
 def end_other(a, b)
-
+  a_down = a.downcase
+  b_down = b.downcase
+  a_len = a_down.length
+  b_len = b_down.length
+  if a_down.slice(-(b_len)..-1) == b_down || b_down.slice(-(a_len)..-1) == a_down
+    puts "True"
+  else
+    puts "False"
+  end
 end
 
 end_other('Hiabc', 'abc') 
